a7c66fae1d5e412c16dc0afadf243c9c414f2966
Alisson <alissonfernandes200@gmail.com>
2024-07-07 15:41:43 -0300

    Mostra visualmente texto criptografado/descriptografado

--- javascript/app.js
+++ javascript/app.js
@@ -6,5 +6,19 @@ function criptografar() {
     .replaceAll('a', 'ai')
     .replaceAll('o', 'ober')
     .replaceAll('u', 'ufat');
-    console.log(textoCriptografado);
+    mostrarTexto(textoCriptografado);
+}
+
+function mostrarTexto(textoCriptografado) {
+    let conteudo1 = document.getElementById('conteudo_1').style;
+    let conteudo2 = document.getElementById('conteudo_2').style;
+    
+    if (textoCriptografado.length != 0) {
+        conteudo1.display= 'none';
+        conteudo2.display= 'flex';
+        document.getElementById('texto_saida').value= textoCriptografado;
+    } else{
+        conteudo1.display= 'flex';
+        conteudo2.display= 'none';
+    }
 }
